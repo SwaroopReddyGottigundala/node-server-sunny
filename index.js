d9684e9fb3d1ec2c22749634dada44a36c1fd3b0
swaroop reddy <reddy.swaroop2016@gmail.com>
2020-03-24 23:25:12 -0500

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '192.168.0.24';
-const port = 3008;
+const hostname = '0.0.0.0';
+const port = 3010;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
